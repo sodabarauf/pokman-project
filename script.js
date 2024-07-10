7073563cb08c671aa9f1c6869ffb9220192e8130
sodabarauf <sodabarauf4@gmail.com>
2024-07-10 10:16:37 +0430

    added linters

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-const userInput = document.getElementById("search-input");
-const submitBtn = document.getElementById("search-button");
-const pokemonImage = document.getElementById("pokemon-image");
+const userInput = document.getElementById('search-input');
+const submitBtn = document.getElementById('search-button');
+const pokemonImage = document.getElementById('pokemon-image');
 
-const pokemonName = document.getElementById("pokemon-name");
-const pokemonId = document.getElementById("pokemon-id");
-const pokemonWeight = document.getElementById("weight");
-const pokemonHeight = document.getElementById("height");
-const pokemonTypes = document.getElementById("types");
+const pokemonName = document.getElementById('pokemon-name');
+const pokemonId = document.getElementById('pokemon-id');
+const pokemonWeight = document.getElementById('weight');
+const pokemonHeight = document.getElementById('height');
+const pokemonTypes = document.getElementById('types');
 
-const hp = document.getElementById("hp");
-const attack = document.getElementById("attack");
-const defense = document.getElementById("defense");
-const specialAttack = document.getElementById("special-attack");
-const specialDefense = document.getElementById("special-defense");
-const speed = document.getElementById("speed");
+const hp = document.getElementById('hp');
+const attack = document.getElementById('attack');
+const defense = document.getElementById('defense');
+const specialAttack = document.getElementById('special-attack');
+const specialDefense = document.getElementById('special-defense');
+const speed = document.getElementById('speed');
 
 const searchPokedex = async () => {
-  if (userInput.value === "") {
+  if (userInput.value === '') {
     return;
   }
 
@@ -24,10 +24,12 @@ const searchPokedex = async () => {
     const res = await fetch(`https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${userInput.value.toLowerCase()}`);
     const data = await res.json();
     console.log(data);
-    const { name, id, weight, height, types, stats, sprites } = data;
+    const { 
+    name, id, weight, height, types, stats, sprites
+    } = data;
 
     pokemonImage.innerHTML = `
-      <img src="${sprites.front_default}" id="sprite">
+      <img src='${sprites.front_default}' id='sprite'>
     `;
 
     pokemonName.innerHTML = name.toUpperCase();
@@ -35,7 +37,7 @@ const searchPokedex = async () => {
 
     pokemonWeight.innerHTML = `Weight: ${weight}`;
     pokemonHeight.innerHTML = `Height: ${height}`;
-    pokemonTypes.innerHTML = types.map(type => `<span class="${type.type.name.toLowerCase()}">${type.type.name.toUpperCase()}</span>`).join(" ");
+    pokemonTypes.innerHTML = types.map((type) => `<span class='${type.type.name.toLowerCase()}'>${type.type.name.toUpperCase()}</span>`).join(' ');
 
     hp.innerHTML = stats[0].base_stat;
     attack.innerHTML = stats[1].base_stat;
@@ -44,15 +46,15 @@ const searchPokedex = async () => {
     specialDefense.innerHTML = stats[4].base_stat;
     speed.innerHTML = stats[5].base_stat;
   }
-  catch(err) {
+  catch (err) {
     console.log(err);
-    alert("Pokemon not found");
+    alert('Pokemon not found');
   }
-}
+};
 
-submitBtn.addEventListener("click", searchPokedex);
-userInput.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
+submitBtn.addEventListener('click', searchPokedex);
+userInput.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
     searchPokedex();
   }
-})
+});
